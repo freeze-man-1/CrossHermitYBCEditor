23e6d4fc0a69f6264731e2e7f75eb735c1fd30a8
freeze-man-1 <c.breuer@gmail.com>
2026-07-08 18:39:55 -0400

    Update editor.py

--- editor.py
+++ editor.py
@@ -562,5 +562,4 @@ def save_script():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True, host='0.0.0.0')
